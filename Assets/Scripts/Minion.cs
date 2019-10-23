7c2ffb3cccb59f6b8982cfbbc8beddddb68003ee
Mercurius A <mercuriusawad7@hotmail.com>
2019-10-23 16:06:52 +1100

    Created a functional turret and minion though scripts

---
+++ Assets/Scripts/Minion.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minion : MonoBehaviour
+{
+    public GameObject turret;
+    public float speed = 5;
+
+
+
+   private void Start()
+   {
+        turret = GameObject.Find("Turret");
+
+
+   }
+
+    
+    void Update()
+    {
+       
+        transform.LookAt(turret.transform.position);
+
+
+        transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/Turret.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+        if(Physics.Raycast(ray, out hit, 100))
+        {
+            transform.LookAt(hit.transform.position);
+            transform.LookAt(new Vector3(hit.point.x, 0, hit.point.z));
+        }
+
+    }
+}
